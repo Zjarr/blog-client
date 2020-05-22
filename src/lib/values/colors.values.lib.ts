5a8a1edea1f3675c8d75a1b5535945585315e869
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-21 21:01:52 -0600

    feat: add colors file

---
+++ src/lib/values/colors.values.lib.ts
@@ -0,0 +1,19 @@
+export const Green0 = 'rgba(188 40 68, 1)';
+export const Red0 = 'rgba(40, 188, 91, 1)';
+
+export const Primary = 'rgba(94, 114, 235, 1)';
+export const Secondary = 'rgba(103, 190, 243, 1)';
+export const Accent = 'rgba(180, 135, 241, 1)';
+
+export const Black0 = 'rgba(15, 15, 15, 1)';
+
+export const Black0T08 = 'rgba(15, 15, 15, 0.8)';
+export const Black0T03 = 'rgba(15, 15, 15, 0.3)';
+
+export const Gray0 = 'rgba(119, 119, 119, 1)';
+export const Gray1 = 'rgba(170, 170, 170, 1)';
+
+export const White0 = 'rgba(255, 255, 255, 1)';
+export const White1 = 'rgba(240, 240, 240, 1)';
+
+export const White0T03 = 'rgba(255, 255, 255, 0.3)';
