4c92ce82e168f35fc711bad093ca96bac86941e4
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-21 22:52:42 -0600

    feat: add custom input component

---
+++ src/components/input/style.input.component.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+import { Gray0, Gray1, White1 } from '../../lib/values';
+
+export const InputBox = styled.input<{ width?: string, disabled: boolean }>`
+  border: 4px solid ${({ disabled }): string => disabled ? 'transparent' : White1};
+  border-radius: 10px;
+  cursor: ${({ disabled }): string => disabled ? 'default' : 'text'};
+  font-family: Proxima Nova Regular;
+  font-size: 16px;
+  height: 40px;
+  margin: 0;
+  outline: none;
+  padding: 0 8px;
+  width: ${({ width }): string => width ? width : 'auto'};
+
+  ::placeholder {
+    color: ${Gray1}
+  }
+`;
+
+export const Label = styled.label`
+  color: ${Gray0};
+  display: block;
+  font-family: Proxima Nova Bold;
+  font-size: 16px;
+  margin: 0 0 4px 12px;
+`;
