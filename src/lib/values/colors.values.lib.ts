4b7727d28ac398fd3bb1310605e6c4a7811455d6
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-23 14:53:44 -0600

    fix: update colors

--- src/lib/values/colors.values.lib.ts
+++ src/lib/values/colors.values.lib.ts
@@ -1,6 +1,4 @@
 export const Black0 = 'rgba(15, 15, 15, 1)';
-export const Black0T03 = 'rgba(15, 15, 15, 0.3)';
-export const Black0T08 = 'rgba(15, 15, 15, 0.8)';
 
 export const Gray0 = 'rgba(119, 119, 119, 1)';
 export const Gray1 = 'rgba(170, 170, 170, 1)';
@@ -17,11 +15,7 @@ export const Purple2 = 'rgba(89, 109, 230, 1)';
 
 export const Red0 = 'rgba(40, 188, 91, 1)';
 
-export const Transparent = 'rgba(0, 0, 0, 0)';
-
 export const White0 = 'rgba(255, 255, 255, 1)';
 export const White1 = 'rgba(245, 245, 245, 1)';
 export const White2 = 'rgba(240, 240, 240, 1)';
 export const White3 = 'rgba(235, 235, 235, 1)';
-
-export const White0T03 = 'rgba(255, 255, 255, 0.3)';
