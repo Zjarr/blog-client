20f26cbf49e0bd7d945e801305e0d98e6ce77aec
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-23 18:09:55 -0600

    fix: update input styles

--- src/components/input/style.input.components.tsx
+++ src/components/input/style.input.components.tsx
@@ -1,28 +1,30 @@
 import styled from 'styled-components';
 
-import { Gray0, Gray1, White1 } from '../../lib/values';
+import { alpha } from '../../lib/functions';
+import { ColorBlack0, ColorGray0, ColorGray1, ColorWhite1, TextNormal } from '../../lib/values';
 
 export const InputContainer = styled.input<{ width?: string, disabled: boolean }>`
-  border: 4px solid ${({ disabled }): string => disabled ? 'transparent' : White1};
+  background-color: ${alpha(ColorBlack0, 0)};
+  border: 4px solid ${({ disabled }): string => disabled ? 'transparent' : ColorWhite1};
   border-radius: 10px;
   cursor: ${({ disabled }): string => disabled ? 'default' : 'text'};
   font-family: Proxima Nova Regular;
-  font-size: 16px;
-  height: 40px;
+  font-size: ${TextNormal};
+  height: 48px;
   margin: 0;
   outline: none;
   padding: 0 8px;
   width: ${({ width }): string => width ? width : 'auto'};
 
   ::placeholder {
-    color: ${Gray1}
+    color: ${ColorGray1}
   }
 `;
 
 export const Label = styled.label`
-  color: ${Gray0};
+  color: ${ColorGray0};
   display: block;
   font-family: Proxima Nova Bold;
-  font-size: 16px;
+  font-size: ${TextNormal};
   margin: 0 0 4px 12px;
 `;
