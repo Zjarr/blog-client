6d83b4af3db6af11fe40053d9809251f86c0bd91
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-23 18:08:54 -0600

    fix: update color names

--- src/lib/values/colors.values.lib.ts
+++ src/lib/values/colors.values.lib.ts
@@ -1,21 +1,22 @@
-export const Black0 = 'rgba(15, 15, 15, 1)';
+export const ColorBlack0 = 'rgba(15, 15, 15, 1)';
+export const ColorBlack1 = 'rgba(0, 0, 0, 1)';
 
-export const Gray0 = 'rgba(119, 119, 119, 1)';
-export const Gray1 = 'rgba(170, 170, 170, 1)';
+export const ColorGray0 = 'rgba(119, 119, 119, 1)';
+export const ColorGray1 = 'rgba(170, 170, 170, 1)';
 
-export const Green0 = 'rgba(188 40 68, 1)';
+export const ColorGreen0 = 'rgba(188 40 68, 1)';
 
-export const LightBlue0 = 'rgba(103, 190, 243, 1)';
+export const ColorLightBlue0 = 'rgba(103, 190, 243, 1)';
 
-export const Magenta0 = 'rgba(180, 135, 241, 1)';
+export const ColorMagenta0 = 'rgba(180, 135, 241, 1)';
 
-export const Purple0 = 'rgba(99, 119, 240, 1)';
-export const Purple1 = 'rgba(94, 114, 235, 1)';
-export const Purple2 = 'rgba(89, 109, 230, 1)';
+export const ColorPurple0 = 'rgba(99, 119, 240, 1)';
+export const ColorPurple1 = 'rgba(94, 114, 235, 1)';
+export const ColorPurple2 = 'rgba(89, 109, 230, 1)';
 
-export const Red0 = 'rgba(40, 188, 91, 1)';
+export const ColorRed0 = 'rgba(40, 188, 91, 1)';
 
-export const White0 = 'rgba(255, 255, 255, 1)';
-export const White1 = 'rgba(245, 245, 245, 1)';
-export const White2 = 'rgba(240, 240, 240, 1)';
-export const White3 = 'rgba(235, 235, 235, 1)';
+export const ColorWhite0 = 'rgba(255, 255, 255, 1)';
+export const ColorWhite1 = 'rgba(245, 245, 245, 1)';
+export const ColorWhite2 = 'rgba(240, 240, 240, 1)';
+export const ColorWhite3 = 'rgba(235, 235, 235, 1)';
