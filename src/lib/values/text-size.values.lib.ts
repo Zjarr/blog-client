29d9f6a90214c6c6aae7ef33471137c12d6ff225
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-24 19:04:47 -0600

    fix: update text names

--- src/lib/values/text-size.values.lib.ts
+++ src/lib/values/text-size.values.lib.ts
@@ -1,4 +1,4 @@
-export const TextExtraBig = '32px';
-export const TextBig = '24px';
-export const TextNormal = '16px';
-export const TextSmall = '8px';
+export const TEXT_EXTRA_BIG = '32px';
+export const TEXT_BIG = '24px';
+export const TEXT_NORMAL = '16px';
+export const TEXT_SMALL = '8px';
