238bcd866c05b7b43b3a64727d29eed6d2f5d379
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-24 19:05:13 -0600

    fix: update input variable imports

--- src/components/input/style.input.components.tsx
+++ src/components/input/style.input.components.tsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 import { alpha } from '../../lib/functions';
-import { ColorBlack0, ColorGray0, ColorGray1, ColorWhite1, TextNormal } from '../../lib/values';
+import { COLOR_BLACK_0, COLOR_BLACK_1, COLOR_GRAY_0, COLOR_GRAY_1, COLOR_WHITE_1, TEXT_NORMAL } from '../../lib/values';
 
 export const InputContainer = styled.input<{ width?: string, disabled: boolean }>`
-  background-color: ${alpha(ColorBlack0, 0)};
-  border: 4px solid ${({ disabled }): string => disabled ? 'transparent' : ColorWhite1};
+  background-color: ${alpha(COLOR_BLACK_0, 0)};
+  border: 4px solid ${({ disabled }): string => disabled ? alpha(COLOR_BLACK_1, 0) : COLOR_WHITE_1};
   border-radius: 10px;
   cursor: ${({ disabled }): string => disabled ? 'default' : 'text'};
   font-family: Proxima Nova Regular;
-  font-size: ${TextNormal};
+  font-size: ${TEXT_NORMAL};
   height: 48px;
   margin: 0;
   outline: none;
@@ -17,14 +17,14 @@ export const InputContainer = styled.input<{ width?: string, disabled: boolean }
   width: ${({ width }): string => width ? width : 'auto'};
 
   ::placeholder {
-    color: ${ColorGray1}
+    color: ${COLOR_GRAY_1}
   }
 `;
 
 export const Label = styled.label`
-  color: ${ColorGray0};
+  color: ${COLOR_GRAY_0};
   display: block;
   font-family: Proxima Nova Bold;
-  font-size: ${TextNormal};
+  font-size: ${TEXT_NORMAL};
   margin: 0 0 4px 12px;
 `;
