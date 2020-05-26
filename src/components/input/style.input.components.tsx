dc2f9717910bf4ea28b95538ed0fc50ca967657f
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-25 20:15:44 -0600

    fix: improve disabled and default input colors

--- src/components/input/style.input.components.tsx
+++ src/components/input/style.input.components.tsx
@@ -1,34 +1,48 @@
 import styled from 'styled-components';
 
 import { alpha } from '../../lib/functions';
-import { COLOR_BLACK_0, COLOR_BLACK_1, COLOR_GRAY_0, COLOR_GRAY_1, COLOR_PURPLE_0, COLOR_WHITE_2, TEXT_NORMAL } from '../../lib/values';
+import { COLOR_BLACK_0, COLOR_BLACK_1, COLOR_GRAY_0, COLOR_GRAY_1, COLOR_PURPLE_0, COLOR_WHITE_1, TEXT_NORMAL } from '../../lib/values';
+
+const getWidth = (width?: string): string => width ? width : 'auto';
 
 export const InputContainer = styled.input<{ width?: string, disabled: boolean }>`
-  background-color: ${alpha(COLOR_BLACK_0, 0)};
-  border: 2px solid ${({ disabled }): string => disabled ? alpha(COLOR_BLACK_1, 0) : COLOR_WHITE_2};
   border-radius: 10px;
-  cursor: ${({ disabled }): string => disabled ? 'default' : 'text'};
+  cursor: text;
   font-family: Proxima Nova Regular;
-  font-size: ${TEXT_NORMAL};
   height: 48px;
   margin: 0;
   outline: none;
   padding: 0 8px;
-  width: ${({ width }): string => width ? width : 'auto'};
+  transition: .125s;
+
+  background-color: ${alpha(COLOR_BLACK_0, 0)};
+  border: 2px solid ${COLOR_WHITE_1};
+  font-size: ${TEXT_NORMAL};
+
+  width: ${({ width }): string => getWidth(width)};
 
   :focus {
+    transition: .125s;
+    
     border-color: ${COLOR_PURPLE_0};
   }
 
   ::placeholder {
-    color: ${COLOR_GRAY_1}
+    color: ${COLOR_GRAY_1};
+  }
+
+  :disabled {
+    border-color: ${alpha(COLOR_BLACK_1, 0)};
+
+    cursor: default;
   }
 `;
 
 export const Label = styled.label`
   color: ${COLOR_GRAY_0};
+  font-size: ${TEXT_NORMAL};
+
   display: block;
   font-family: Proxima Nova Bold;
-  font-size: ${TEXT_NORMAL};
   margin: 0 0 4px 12px;
 `;
