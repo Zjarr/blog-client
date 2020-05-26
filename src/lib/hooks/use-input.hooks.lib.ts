6ff4d6943a6c02aa4530e027dfc2e556b717eb95
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-25 20:15:00 -0600

    fix: update input hook to allow checkboxes

--- src/lib/hooks/use-input.hooks.lib.ts
+++ src/lib/hooks/use-input.hooks.lib.ts
@@ -1,19 +1,23 @@
 import React from 'react';
 
-export const useInput = (initialValue: string | number): IInputHook => {
-  const [value, setValue] = React.useState<string | number>(initialValue);
+export const useInput = (inputValue: string, checkValue = false): IInputHook => {
+  const [checked, setChecked] = React.useState<boolean>(checkValue);
+  const [value, setValue] = React.useState<string>(inputValue);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setChecked(e.target.checked);
     setValue(e.target.value);
   };
 
   return {
+    checked,
     onChange,
     value
   };
 };
 
 export interface IInputHook {
+  checked: boolean;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  value: string | number;
+  value: string;
 }
