bd5fdfaecec0b3d0442a776b8bc80f43dd4fd54c
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-25 20:14:34 -0600

    fix: fix colors

--- src/lib/values/colors.values.lib.ts
+++ src/lib/values/colors.values.lib.ts
@@ -4,7 +4,7 @@ export const COLOR_BLACK_1 = 'rgba(0, 0, 0, 1)';
 export const COLOR_GRAY_0 = 'rgba(119, 119, 119, 1)';
 export const COLOR_GRAY_1 = 'rgba(170, 170, 170, 1)';
 
-export const COLOR_GREEN_0 = 'rgba(188 40 68, 1)';
+export const COLOR_GREEN_0 = 'rgba(40, 188, 91, 1)';
 
 export const COLOR_LIGHTBLUE_0 = 'rgba(103, 190, 243, 1)';
 
@@ -12,9 +12,7 @@ export const COLOR_MAGENTA_0 = 'rgba(180, 135, 241, 1)';
 
 export const COLOR_PURPLE_0 = 'rgba(99, 119, 240, 1)';
 
-export const COLOR_RED_0 = 'rgba(40, 188, 91, 1)';
+export const COLOR_RED_0 = 'rgba(188, 40, 68, 1)';
 
 export const COLOR_WHITE_0 = 'rgba(255, 255, 255, 1)';
-export const COLOR_WHITE_1 = 'rgba(245, 245, 245, 1)';
-export const COLOR_WHITE_2 = 'rgba(240, 240, 240, 1)';
-export const COLOR_WHITE_3 = 'rgba(235, 235, 235, 1)';
+export const COLOR_WHITE_1 = 'rgba(230, 230, 230, 1)';
