efda3e23a367008f6dd6a4a3966db6e3962623e5
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-27 18:19:46 -0600

    fix: update text style variable names

--- src/components/text/style.label.text.components.tsx
+++ src/components/text/style.label.text.components.tsx
@@ -7,7 +7,7 @@ const DEFAULT_MARGIN = '0 0 4px 12px';
 const getTextColor = (color?: string): string => color ? color : COLOR_GRAY_0;
 const getTextMargin = (margin?: string): string => margin ? margin : DEFAULT_MARGIN;
 
-export const Label = styled.label<{ color?: string, margin?: string }>`
+export const LabelContainer = styled.label<{ color?: string, margin?: string }>`
   font-family: Proxima Nova Bold;
   font-size: 16px;
 
