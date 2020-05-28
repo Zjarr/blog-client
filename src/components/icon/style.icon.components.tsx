1b56de736aefe307990ea1abcd21a003249625b3
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-27 19:20:37 -0600

    fix: update icon style file methods

--- src/components/icon/style.icon.components.tsx
+++ src/components/icon/style.icon.components.tsx
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
+const DEFAULT_ICON_COLOR = 'inherit';
+const DEFAULT_ICON_SIZE = '16px';
+
+const getIconColor = (color?: string): string => color ? color : DEFAULT_ICON_COLOR;
+const getIconSize = (size?: string): string => size ? size : DEFAULT_ICON_SIZE;
+
 export const IconContainer = styled.i<{ color?: string, size?: string }>`
-  color: ${({ color }): string => color ? color : 'inherit'};
+  color: ${({ color }): string => getIconColor(color)};
   direction: ltr;
   display: inline-block;
   font-family: Material Icons;
-  font-size: ${({ size }): string => size ? size : '16px'};
+  font-size: ${({ size }): string => getIconSize(size)};
   font-style: normal;
   font-weight: normal;
   line-height: 1;
