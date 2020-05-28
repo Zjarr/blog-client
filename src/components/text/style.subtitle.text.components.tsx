0480c5aa632fc3d266ceeaa9110756b90f6e10b4
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-27 18:04:26 -0600

    feat: add text component

---
+++ src/components/text/style.subtitle.text.components.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+import { COLOR_BLACK_0 } from '../../lib/values';
+
+const DEFAULT_MARGIN = '0px';
+
+const getTextColor = (color?: string): string => color ? color : COLOR_BLACK_0;
+const getTextMargin = (margin?: string): string => margin ? margin : DEFAULT_MARGIN;
+
+export const SubTitle = styled.label<{ color?: string, margin?: string }>`
+  font-family: Proxima Nova Bold;
+  font-size: 24px;
+
+  color: ${({ color }): string => getTextColor(color)};
+  margin: ${({ margin }): string => getTextMargin(margin)};
+`;
