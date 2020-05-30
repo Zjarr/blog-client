9816433cf80ac5388fc0f4a5ffbc08cb0a6b8a1a
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-30 16:21:40 -0600

    chore: update node version

---
+++ src/components/dropdown/style.dropdown.components.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const DropdownContainer = styled.div``;
+
+export const DropdownTrigger = styled.button``;
+
+export const DropdownBody = styled.div``;
+
+export const DropdownItem = styled.div``;
