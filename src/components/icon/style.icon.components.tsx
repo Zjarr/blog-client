8e903bea6aa3f6fbcfdd3a72a3ed4ab8c16aa45a
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-05-30 22:16:12 -0600

    fix: use default size for icons

--- src/components/icon/style.icon.components.tsx
+++ src/components/icon/style.icon.components.tsx
@@ -7,18 +7,22 @@ const getIconColor = (color?: string): string => color ? color : DEFAULT_ICON_CO
 const getIconSize = (size?: string): string => size ? size : DEFAULT_ICON_SIZE;
 
 export const IconContainer = styled.i<{ color?: string, size?: string }>`
+  align-items: center;
   color: ${({ color }): string => getIconColor(color)};
   direction: ltr;
-  display: inline-block;
+  display: flex;
   font-family: Material Icons;
   font-size: ${({ size }): string => getIconSize(size)};
   font-style: normal;
   font-weight: normal;
+  height: ${({ size }): string => getIconSize(size)};
+  justify-content: center;
   line-height: 1;
   letter-spacing: normal;
   text-rendering: optimizeLegibility;
   text-transform: none;
   white-space: nowrap;
+  width: ${({ size }): string => getIconSize(size)};
   word-wrap: normal;
   -webkit-font-smoothing: antialiased;
 `;
