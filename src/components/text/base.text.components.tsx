bf1937796c3870a97abaff17dde9ccdf049b00b0
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-03 19:52:34 -0600

    fix: refactor text component

--- src/components/text/base.text.components.tsx
+++ src/components/text/base.text.components.tsx
@@ -8,46 +8,51 @@ import { SubTitle, SubTitleContainer } from './style.subtitle.text.components';
 import { Title, TitleContainer } from './style.title.text.components';
 
 export const Text: React.FC<IProps> = ({ icon, type, children, ...rest }) => {
-  return (
-    <>
-      {
-        type === 'title' &&
-        <TitleContainer {...rest}>
-          {
-            icon && <Icon name={icon} />
-          }
-          <Title>{children}</Title>
-        </TitleContainer>
-      }
-      {
-        type === 'subtitle' &&
-        <SubTitleContainer {...rest}>
-          {
-            icon && <Icon name={icon} />
-          }
-          <SubTitle>{children}</SubTitle>
-        </SubTitleContainer>
-      }
-      {
-        type === 'paragraph' &&
-        <ParagraphContainer {...rest}>
-          {
-            icon && <Icon name={icon} />
-          }
-          <Paragraph>{children}</Paragraph>
-        </ParagraphContainer>
-      }
-      {
-        type === 'label' &&
-        <LabelContainer {...rest}>
-          {
-            icon && <Icon name={icon} />
-          }
-          <Label>{children}</Label>
-        </LabelContainer>
-      }
-    </>
-  );
+  if (type === 'title') {
+    return (
+      <TitleContainer {...rest}>
+        {
+          icon && <Icon name={icon} />
+        }
+        <Title>{children}</Title>
+      </TitleContainer>
+    );
+  }
+
+  if (type === 'subtitle') {
+    return (
+      <SubTitleContainer {...rest}>
+        {
+          icon && <Icon name={icon} />
+        }
+        <SubTitle>{children}</SubTitle>
+      </SubTitleContainer>
+    );
+  }
+
+  if (type === 'paragraph') {
+    return (
+      <ParagraphContainer {...rest}>
+        {
+          icon && <Icon name={icon} />
+        }
+        <Paragraph>{children}</Paragraph>
+      </ParagraphContainer>
+    );
+  }
+
+  if (type === 'label') {
+    return (
+      <LabelContainer {...rest}>
+        {
+          icon && <Icon name={icon} />
+        }
+        <Label>{children}</Label>
+      </LabelContainer>
+    );
+  }
+
+  return null;
 };
 
 interface IProps {
