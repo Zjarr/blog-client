eea698c6ecb86ecb78d84f08c9a1f8810939e076
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-08 01:28:35 -0600

    fix: update banner styles

--- src/components/banner/style.banner.components.tsx
+++ src/components/banner/style.banner.components.tsx
@@ -1,39 +1,41 @@
 import styled from 'styled-components';
 
-import { COLOR_BLACK_0, COLOR_BLACK_0_50, COLOR_GRAY_0, COLOR_WHITE_0 } from '../../lib/values';
+import { COLOR_BLACK, COLOR_BLACK_5, COLOR_GRAY_MEDIUM, COLOR_WHITE } from '../../lib/values';
 
-const BANNER_VISIBLE_OPACITY = '1';
-const BANNER_INVISIBLE_OPACITY = '0';
+const DEFAULT_BANNER_OPACITY = '0';
+const DEFAULT_BANNER_TRANSFORM = 'translateX(0)';
 
-const BANNER_VISIBLE_TRANSFORM = 'translateX(-380px)';
-const BANNER_INVISIBLE_TRANSFORM = 'translateX(0)';
+const VISIBLE_BANNER_OPACITY = '1';
+const VISIBLE_BANNER_TRANSFORM = 'translateX(-380px)';
 
-const getBannerOpacity = (isVisible?: boolean): string => isVisible ? BANNER_VISIBLE_OPACITY : BANNER_INVISIBLE_OPACITY;
-const getBannerTransform = (isVisible?: boolean): string => isVisible ? BANNER_VISIBLE_TRANSFORM : BANNER_INVISIBLE_TRANSFORM;
+const getBannerOpacity = (isVisible?: boolean): string => isVisible ? VISIBLE_BANNER_OPACITY : DEFAULT_BANNER_OPACITY;
+const getBannerTransform = (isVisible?: boolean): string => isVisible ? VISIBLE_BANNER_TRANSFORM : DEFAULT_BANNER_TRANSFORM;
 
-const getIconColor = (color?: string): string => color ? color : COLOR_GRAY_0;
+const getIconColor = (color?: string): string => color || COLOR_GRAY_MEDIUM;
 
 export const BannerContainer = styled.div<{ isVisible: boolean }>`
   align-items: center;
   border-radius: 10px;
   display: flex;
-  height: 32px;
-  overflow: hidden;
+  min-height: 48px;
   padding: 8px 16px;
-  position: absolute;
+  position: fixed;
   right: -332px;
-  text-overflow: ellipsis;
+  text-align: justify;
   top: 48px;
-  white-space: nowrap;
   transition: .5s ease;
   width: 300px;
 
-  background-color: ${COLOR_BLACK_0};
-  box-shadow: 0 0 8px 0px ${COLOR_BLACK_0_50};
-  color: ${COLOR_WHITE_0};
+  background-color: ${COLOR_BLACK};
+  box-shadow: 0 0 8px 0px ${COLOR_BLACK_5};
+  color: ${COLOR_WHITE};
 
   opacity:  ${({ isVisible }): string => getBannerOpacity(isVisible)};
   transform: ${({ isVisible }): string => getBannerTransform(isVisible)};
+
+  div:last-child{
+    width: calc(100% - 48px);
+  }
 `;
 
 export const IconContainer = styled.div<{ color?: string }>`
@@ -45,7 +47,7 @@ export const IconContainer = styled.div<{ color?: string }>`
   margin-right: 16px;
   width: 32px;
 
-  background-color: ${COLOR_WHITE_0};
+  background-color: ${COLOR_WHITE};
 
   i {
     color: ${({ color }): string => getIconColor(color)};
