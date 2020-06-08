f5121047e1e69051db79d3c8d602dc7168c0d1e9
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-08 01:22:28 -0600

    feat: update global styles

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 import './assets/fonts/material-icons/index.css';
 import './assets/fonts/proxima-nova/index.css';
-import './styles/index.css';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
