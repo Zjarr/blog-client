93928a11710721a80e18f9ddc23c82f5de4acba5
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-08 01:30:46 -0600

    fix: update text styles

--- src/components/text/style.label.text.components.tsx
+++ src/components/text/style.label.text.components.tsx
@@ -1,10 +1,10 @@
 import styled from 'styled-components';
 
-import { COLOR_GRAY_0 } from '../../lib/values';
+import { COLOR_GRAY_DARK } from '../../lib/values';
 
 const DEFAULT_MARGIN = '0 0 4px 12px';
 
-const getTextColor = (color?: string): string => color ? color : COLOR_GRAY_0;
+const getTextColor = (color?: string): string => color ? color : COLOR_GRAY_DARK;
 const getTextMargin = (margin?: string): string => margin ? margin : DEFAULT_MARGIN;
 
 export const LabelContainer = styled.div<{ color?: string, margin?: string }>`
@@ -20,7 +20,7 @@ export const LabelContainer = styled.div<{ color?: string, margin?: string }>`
   }
 `;
 
-export const Label = styled.label<{}>`
+export const Label = styled.label`
   font-family: Proxima Nova Bold;
   font-size: 16px;
   margin: 0;
