a79cb5cb1fe5c7c36cafe0b1a99126760cec7c34
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-11 23:23:10 -0600

    feat: modal component

--- src/components/modal/base.modal.components.tsx
+++ src/components/modal/base.modal.components.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 
-export const Modal: React.FC<IProps> = () => {
+import { COLOR_BLACK_0 } from '../../lib/values';
+
+import { CloseButton, BodyContainer, ModalContainer } from './style.modal.components';
+
+export const Modal: React.FC<IProps> = ({ body, onClose, visible }) => {
+  const handleCloseRequest = (): void => {
+    return onClose && onClose();
+  };
+
   return (
-    <></>
+    <ModalContainer visible={visible}>
+      <CloseButton
+        color={COLOR_BLACK_0}
+        icon={'close'}
+        type={'color'}
+
+        onClick={handleCloseRequest}
+      />
+      <BodyContainer>
+        {body}
+      </BodyContainer>
+    </ModalContainer>
   );
 };
 
-interface IProps { }
+interface IProps {
+  body: ReactElement;
+  onClose?: () => void;
+  visible: boolean;
+}
--- src/components/modal/style.modal.components.tsx
+++ src/components/modal/style.modal.components.tsx
@@ -1,3 +1,34 @@
 import Styled from 'styled-components';
 
-export const ModalContainer = Styled.div``;
+import { COLOR_BLACK_8 } from '../../lib/values';
+
+import { Button } from '../button';
+
+const DEFAULT_CONTAINER_DISPLAY = 'none';
+
+const VISIBLE_CONTAINER_DISPLAY = 'flex';
+
+const getContainerVisible = (visible?: boolean): string => visible ? VISIBLE_CONTAINER_DISPLAY : DEFAULT_CONTAINER_DISPLAY;
+
+export const ModalContainer = Styled.div<{ visible: boolean }>`
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 1;
+
+  background-color: ${COLOR_BLACK_8};
+
+  display: ${({ visible }): string => getContainerVisible(visible)};
+`;
+
+export const BodyContainer = Styled.div``;
+
+export const CloseButton = Styled(Button)`
+position: absolute;
+right: 16px;
+top: 16px;
+`;
