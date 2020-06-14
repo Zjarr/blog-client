c40a116fd5282789417189d87da5fb41f2b888bb
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-14 01:46:18 -0600

    feat: add base files for loading modal

---
+++ src/components/loading/base.loading.components.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import BarLoader from 'react-spinners/BarLoader';
+
+import { COLOR_WHITE } from '../../lib/values';
+
+import { Modal } from '../modal';
+import { Text } from '../text';
+
+import { SpinnerContainer } from './style.loading.components';
+
+export const Loading: React.FC<IProps> = ({ text, visible }) => {
+  return (
+    <Modal onClose={(): void => { }} visible={visible}>
+      <SpinnerContainer>
+        <BarLoader color={'white'} width={288} height={6} />
+      </SpinnerContainer>
+
+      <Text type={'paragraph'} color={COLOR_WHITE}>
+        {text}
+      </Text>
+    </Modal>
+  );
+};
+
+interface IProps {
+  text?: string;
+  visible: boolean;
+}
---
+++ src/components/loading/style.loading.components.tsx
@@ -0,0 +1,7 @@
+import Styled from 'styled-components';
+
+export const SpinnerContainer = Styled.div`
+  border-radius: 3px;
+  margin-bottom: 16px;
+  overflow: hidden;
+`;
