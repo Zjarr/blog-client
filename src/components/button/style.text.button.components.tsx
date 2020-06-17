858b21b3854dd574fbad73d682d85d98d4c9c034
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-17 00:03:05 -0600

    feat: add color parameter to text button

--- src/components/button/style.text.button.components.tsx
+++ src/components/button/style.text.button.components.tsx
@@ -2,16 +2,19 @@ import Styled from 'styled-components';
 
 import { COLOR_BLACK, COLOR_BLACK_0, COLOR_GRAY_LIGHT, TEXT_NORMAL } from '../../lib/values';
 
+const DEFAULT_CONTAINER_COLOR = COLOR_BLACK;
 const DEFAULT_CONTAINER_HEIGHT = '32px';
 const DEFAULT_CONTAINER_TEXT_ALIGN = 'center';
 const DEFAULT_CONTAINER_WIDTH = 'auto';
 
+const getContainerColor = (color?: string): string => color ? color : DEFAULT_CONTAINER_COLOR;
 const getContainerHeight = (height?: string): string => height ? height : DEFAULT_CONTAINER_HEIGHT;
 const getContainerTextAlign = (align?: string): string => align ? align : DEFAULT_CONTAINER_TEXT_ALIGN;
 const getContainerWidth = (width?: string): string => width ? width : DEFAULT_CONTAINER_WIDTH;
 
 export const TextButtonContainer = Styled.button<{
   align?: string,
+  color?: string,
   disabled?: boolean,
   height?: string,
   iconOnly?: boolean,
@@ -27,9 +30,9 @@ export const TextButtonContainer = Styled.button<{
 
   background-color: ${COLOR_BLACK_0};
   border: 2px solid ${COLOR_BLACK_0};
-  color: ${COLOR_BLACK};
   font-size: ${TEXT_NORMAL};
 
+  color: ${({ color }): string => getContainerColor(color)};
   height: ${({ height }): string => getContainerHeight(height)};
   justify-content: ${({ align }): string => getContainerTextAlign(align)};
   width: ${({ width }): string => getContainerWidth(width)};
