300b94e7830c777d69578587d9ab7fcce6e0a28e
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-17 20:41:02 -0600

    fix: update list component style

--- src/components/list/base.list.components.tsx
+++ src/components/list/base.list.components.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+
+import { useInput } from '../../lib/hooks';
 
 import { Card, ICard } from '../card';
+import { Input } from '../input';
 
-import { CardContainer, ListContainer } from './style.list.components';
+import { CardContainer, ListContainer, SearchContainer } from './style.list.components';
 
-export const List: React.FC<IList> = ({ cards }) => {
+export const List: React.FC<IList> = ({ cards, onSearch }) => {
+  const searchInput = useInput('');
+
+  const handleSearchOnChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    if (onSearch) {
+      onSearch(e.target.value);
+    }
+
+    return searchInput.onChange(e);
+  };
 
   const getCardItems = (cards: ICard[]): JSX.Element[] => {
     return cards.map((card, index) =>
@@ -16,13 +29,20 @@ export const List: React.FC<IList> = ({ cards }) => {
 
   return (
     <ListContainer>
-      {
-        cards && getCardItems(cards)
-      }
+      <SearchContainer>
+        <Input icon={'search'} placeholder={'Search by name'} type={'text'} value={searchInput.value} onChange={handleSearchOnChange} />
+      </SearchContainer>
+
+      <Row>
+        {
+          cards && getCardItems(cards)
+        }
+      </Row>
     </ListContainer>
   );
 };
 
 interface IList {
   cards: ICard[];
+  onSearch?: (searchString: string) => void
 };
--- src/components/list/style.list.components.tsx
+++ src/components/list/style.list.components.tsx
@@ -1,11 +1,22 @@
 import Styled from 'styled-components';
 
 import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-
-export const ListContainer = Styled(Row)`
-`;
 
 export const CardContainer = Styled(Col)`
   margin-bottom: 32px;
+`;
+
+export const ListContainer = Styled.div`
+`;
+
+export const SearchContainer = Styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 32px;
+  width: 100%;
+
+  input {
+    margin-right: 32px;
+    width: 400px;
+  }
 `;
