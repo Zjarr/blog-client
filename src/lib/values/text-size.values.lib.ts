2e6d5a64e6236e5e3b5e488ecfbb70848346bde1
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-20 21:12:02 -0600

    feat: add client text sizes

--- src/lib/values/text-size.values.lib.ts
+++ src/lib/values/text-size.values.lib.ts
@@ -1,4 +1,7 @@
-export const TEXT_EXTRA_BIG = '32px';
 export const TEXT_BIG = '24px';
+export const TEXT_EXTRA_BIG = '32px';
 export const TEXT_NORMAL = '16px';
 export const TEXT_SMALL = '8px';
+
+export const TEXT_CLIENT_BIG = '30px';
+export const TEXT_CLIENT_NORMAL = '20px';
