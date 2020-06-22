2ff2495756f44bdf7315094162fcfe6e6458764a
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-21 19:10:09 -0600

    feat: add clipboard feature to cards

--- src/components/card/base.card.components.tsx
+++ src/components/card/base.card.components.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { COLOR_BLACK_0, COLOR_GRAY_DARK, COLOR_GREEN, COLOR_WHITE } from '../../lib/values';
 
-import { COLOR_GRAY_DARK } from '../../lib/values';
-
+import { Banner } from '../banner';
 import { Button } from '../button';
 import { Icon } from '../icon';
 import { Image } from '../image';
@@ -15,51 +15,90 @@ import {
 } from './style.icon.card.components';
 
 import {
+  ClipboardContainer,
   ImageCardContainer,
   ImageContainer,
   ImageFinalContainer,
   ImageMiddleContainer,
   ImageSecondaryTextContainer,
-  ImageStateIndicator
+  ImageStateIndicator,
+  URLContainer
 } from './style.image.card.components';
 
-export const Card: React.FC<ICard> = ({ active, disabled, icon, image, link, onClick, secondaryText, text, title, type, ...rest }) => {
+export const Card: React.FC<ICard> = ({ active, clipboard, disabled, icon, image, link, onClick, secondaryText, text, title, type, ...rest }) => {
+  const [bannerVisible, setBannerVisible] = React.useState<boolean>(false);
+
   const handleButtonClick = (): void => {
     return onClick && onClick();
   };
 
+  const handleCopyClipboard = (link?: string): void => {
+    const el = document.createElement('input');
+
+    el.value = link || '';
+    document.body.appendChild(el);
+
+    el.select();
+
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    return setBannerVisible(true);
+  };
+
   if (type === 'image') {
     return (
-      <ImageCardContainer {...rest}>
-        <ImageContainer>
-          {
-            icon ?
-              <Icon name={icon} size={'48px'} /> :
-              <Image type={'circle'} height={'96px'} width={'96px'} src={image} />
-          }
-        </ImageContainer>
-
-        <ImageMiddleContainer>
-          {
-            title && <Text type={'subtitle'}>{title}</Text>
-          }
-          <ImageSecondaryTextContainer>
+      <>
+        <ImageCardContainer {...rest}>
+          <ImageContainer>
             {
-              text && <Text type={'paragraph'}>{text}</Text>
+              icon ?
+                <Icon name={icon} size={'48px'} /> :
+                <Image type={'circle'} height={'96px'} width={'96px'} src={image} />
             }
-            {
-              secondaryText && <Text type={'paragraph'}>{secondaryText}</Text>
-            }
-          </ImageSecondaryTextContainer>
-        </ImageMiddleContainer>
+          </ImageContainer>
 
-        <ImageFinalContainer>
-          <ImageStateIndicator active={active} />
+          <ImageMiddleContainer>
+            {
+              title && <Text type={'subtitle'}>{title}</Text>
+            }
+            <ImageSecondaryTextContainer>
+              {
+                text && <Text type={'paragraph'}>{text}</Text>
+              }
+              {
+                secondaryText && <Text type={'paragraph'}>{secondaryText}</Text>
+              }
+            </ImageSecondaryTextContainer>
+          </ImageMiddleContainer>
+
+          <ImageFinalContainer>
+            <ImageStateIndicator active={active} />
+            {
+              link && !clipboard &&
+              <Button as={'a'} href={link} type={'text'} color={COLOR_GRAY_DARK}>View</Button>
+            }
+          </ImageFinalContainer>
+
           {
-            link && <Button as={'a'} href={link} type={'text'} color={COLOR_GRAY_DARK}>View</Button>
+            clipboard &&
+            <ClipboardContainer>
+              <Text type={'label'} color={COLOR_WHITE}>URL:</Text>
+              <URLContainer>
+                <Text type={'paragraph'} color={COLOR_WHITE}>{link}</Text>
+                <Button type={'color'} icon={'file_copy'} color={COLOR_BLACK_0} onClick={(): void => handleCopyClipboard(link)} />
+              </URLContainer>
+            </ClipboardContainer>
           }
-        </ImageFinalContainer>
-      </ImageCardContainer>
+        </ImageCardContainer>
+
+        <Banner
+          color={COLOR_GREEN}
+          icon={'check'}
+          text={'URL has been copied to clipboard'}
+          visible={bannerVisible}
+          onHide={(): void => setBannerVisible(false)} />
+      </>
     );
   }
 
