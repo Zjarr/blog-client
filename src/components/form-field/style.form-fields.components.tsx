d274adfb75839b3c3bdffc2d00b85320883201de
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-22 20:54:27 -0600

    fix: update form-field default width

--- src/components/form-field/style.form-fields.components.tsx
+++ src/components/form-field/style.form-fields.components.tsx
@@ -1,12 +1,12 @@
 import Styled from 'styled-components';
 
-const DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM = '0px';
+const DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM = '24px';
 const DEFAULT_FORM_FIELD_CONTAINER_WIDTH = '100%';
 
-const FORM_FIELD_CONTAINER_MARGIN_BOTTOM = '24px';
+const FORM_FIELD_CONTAINER_NO_MARGIN_BOTTOM = '0px';
 
-const getFormFieldContainerMarginBottom = (marginBottom?: boolean): string =>
-  marginBottom ? FORM_FIELD_CONTAINER_MARGIN_BOTTOM : DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM;
+const getFormFieldContainerMarginBottom = (noMarginBottom?: boolean): string =>
+  noMarginBottom ? FORM_FIELD_CONTAINER_NO_MARGIN_BOTTOM : DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM;
 
 const getFormFieldContainerWidth = (width?: string): string => width ? width : DEFAULT_FORM_FIELD_CONTAINER_WIDTH;
 
@@ -26,9 +26,9 @@ export const BodyContainer = Styled.div`
   }
 `;
 
-export const FormFieldContainer = Styled.div<{ marginBottom?: boolean, width?: string }>`
+export const FormFieldContainer = Styled.div<{ noMarginBottom?: boolean, width?: string }>`
   position: relative;
 
-  margin-bottom: ${({ marginBottom }): string => getFormFieldContainerMarginBottom(marginBottom)};
+  margin-bottom: ${({ noMarginBottom }): string => getFormFieldContainerMarginBottom(noMarginBottom)};
   width: ${({ width }): string => getFormFieldContainerWidth(width)};
 `;
