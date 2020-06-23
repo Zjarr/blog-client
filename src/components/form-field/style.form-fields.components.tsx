2c4870c8e10d5c19eeb740ec8ca06a614f30abd6
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-22 21:15:49 -0600

    fix: update form-field parameter names

--- src/components/form-field/style.form-fields.components.tsx
+++ src/components/form-field/style.form-fields.components.tsx
@@ -5,8 +5,8 @@ const DEFAULT_FORM_FIELD_CONTAINER_WIDTH = '100%';
 
 const FORM_FIELD_CONTAINER_NO_MARGIN_BOTTOM = '0px';
 
-const getFormFieldContainerMarginBottom = (noMarginBottom?: boolean): string =>
-  noMarginBottom ? FORM_FIELD_CONTAINER_NO_MARGIN_BOTTOM : DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM;
+const getFormFieldContainerMarginBottom = (noMargin?: boolean): string =>
+  noMargin ? FORM_FIELD_CONTAINER_NO_MARGIN_BOTTOM : DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM;
 
 const getFormFieldContainerWidth = (width?: string): string => width ? width : DEFAULT_FORM_FIELD_CONTAINER_WIDTH;
 
@@ -26,9 +26,9 @@ export const BodyContainer = Styled.div`
   }
 `;
 
-export const FormFieldContainer = Styled.div<{ noMarginBottom?: boolean, width?: string }>`
+export const FormFieldContainer = Styled.div<{ noMargin?: boolean, width?: string }>`
   position: relative;
 
-  margin-bottom: ${({ noMarginBottom }): string => getFormFieldContainerMarginBottom(noMarginBottom)};
+  margin-bottom: ${({ noMargin }): string => getFormFieldContainerMarginBottom(noMargin)};
   width: ${({ width }): string => getFormFieldContainerWidth(width)};
 `;
