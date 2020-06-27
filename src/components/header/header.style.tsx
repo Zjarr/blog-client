a592f51d85f5c6ca7f9f3089d10a5a18df23bd29
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-27 16:51:07 -0600

    fix: update header style

--- src/components/header/header.style.tsx
+++ src/components/header/header.style.tsx
@@ -2,11 +2,12 @@ import Styled from 'styled-components';
 
 export const HeaderContainer = Styled.div`
   height: 80px;
+  margin-bottom: 32px;
   position: relative;
 `;
 
 export const TitleContainer = Styled.div`
+  bottom: 0;
   padding-left: 16px;
   position: absolute;
-  bottom: 0;
 `;
