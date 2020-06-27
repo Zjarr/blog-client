9c99e12f1143e2bef862ae5dc17b3fc15f965483
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-27 11:52:20 -0600

    feat: add not found page

--- src/pages/not-found/not-found.page.tsx
+++ src/pages/not-found/not-found.page.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import NotFoundWaves from '../../assets/images/not-found-waves.png';
+import { SimpleButton } from '../../components/button';
+import { COLOR_PURPLE, COLOR_GRAY_DARK } from '../../utils/values';
 
-export const NotFoundPage: React.FC<{}> = () => {
+import {
+  BodyContainer,
+  ButtonContainer,
+  NotFoundContainer,
+  TextContainer,
+  Title,
+  WavesContainer,
+  WavesImage
+} from './not-found.style';
+
+import { TitleText, ParagraphText } from '../../components/text';
+
+export const NotFoundPage: React.FC<INotFoundPage> = () => {
   return (
-    <>
-      <h1>Not found</h1>
-    </>
+    <NotFoundContainer>
+      <BodyContainer>
+        <TextContainer>
+          <Title>404!</Title>
+          <TitleText>We miss you. Come back home!</TitleText>
+          <ParagraphText color={COLOR_GRAY_DARK}>Maybe we forgot to add something or had to remove it.</ParagraphText>
+        </TextContainer>
+
+        <ButtonContainer>
+          <SimpleButton color={COLOR_PURPLE} width={'auto'}>Go home</SimpleButton>
+        </ButtonContainer>
+      </BodyContainer>
+
+      <WavesContainer>
+        <WavesImage src={NotFoundWaves} />
+      </WavesContainer>
+    </NotFoundContainer>
   );
 };
+
+interface INotFoundPage {
+
+}
