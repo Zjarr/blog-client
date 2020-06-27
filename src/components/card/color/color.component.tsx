00db60f7cfbef9a4d296f00262a3739e0f9e6487
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-27 16:48:52 -0600

    feat: add color card

---
+++ src/components/card/color/color.component.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { useNavigateTo } from '../../../utils/hooks';
+import { COLOR_WHITE } from '../../../utils/values';
+
+import { TextButton } from '../../button';
+import { Icon } from '../../icon';
+
+import { ButtonContainer, ColorContainer, IconContainer, MiddleContainer, Name, Number } from './color.style';
+
+export const ColorCard: React.FC<IColorCard> = ({ button, color, icon, link, name, number }) => {
+  const navigateTo = useNavigateTo();
+
+  return (
+    <ColorContainer color={color}>
+      <IconContainer>
+        <Icon name={icon} />
+      </IconContainer>
+
+      <MiddleContainer>
+        <Number>{number}</Number>
+        <Name>{name}</Name>
+      </MiddleContainer>
+
+      <ButtonContainer>
+        <TextButton onClick={(): void => navigateTo(link)} color={COLOR_WHITE}>{button}</TextButton>
+      </ButtonContainer>
+    </ColorContainer>
+  );
+};
+
+interface IColorCard {
+  button: string;
+  color: string;
+  icon: string;
+  link: string;
+  name: string;
+  number: number;
+}
---
+++ src/components/card/color/color.style.tsx
@@ -0,0 +1,46 @@
+import Styled from 'styled-components';
+
+import { BORDER_RADIUS_SMALL, COLOR_PURPLE, COLOR_WHITE, COLOR_WHITE_5 } from '../../../utils/values';
+
+const DEFAULT_COLOR_CONTAINER_COLOR = COLOR_PURPLE;
+
+const getColorContainerColor = (color: string): string => color ? color : DEFAULT_COLOR_CONTAINER_COLOR;
+
+export const ColorContainer = Styled.div<{ color: string }>`
+  padding: 16px;
+  height: 100%;
+  width: 100%;
+
+  border-radius: ${BORDER_RADIUS_SMALL};
+  color: ${COLOR_WHITE};
+
+  background-color: ${({ color }): string => getColorContainerColor(color)};
+`;
+
+export const IconContainer = Styled.div``;
+
+export const MiddleContainer = Styled.div`
+  align-items: flex-end;
+  display: flex;
+  flex-direction: column;
+  margin: 48px 0;
+  width: 100%;
+`;
+
+export const Number = Styled.p`
+  margin-bottom: 0;
+  font-size: 80px;
+`;
+
+export const Name = Styled.p`
+  margin-bottom: 0;
+  margin-top: -24px;
+
+  color: ${COLOR_WHITE_5};
+`;
+
+export const ButtonContainer = Styled.div`
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+`;
