f4034d8fbbb642c0f40170a693fdcd4cebc53a86
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-27 21:35:10 -0600

    fix: update header top margin

--- src/components/header/header.style.tsx
+++ src/components/header/header.style.tsx
@@ -2,7 +2,8 @@ import Styled from 'styled-components';
 
 export const HeaderContainer = Styled.div`
   height: 80px;
-  margin-bottom: 16px;
+  margin-bottom: 24px;
+  margin-top: 8px;
   position: relative;
 `;
 
