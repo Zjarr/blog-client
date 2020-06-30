521874454975bf7bc3e4abca19d9ce4aadff7c99
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-30 12:01:10 -0600

    feat: add height param to form-field

--- src/components/form-field/form-fields.style.tsx
+++ src/components/form-field/form-fields.style.tsx
@@ -1,6 +1,7 @@
 import Styled from 'styled-components';
 import Row from 'react-bootstrap/Row';
 
+const DEFAULT_FORM_FIELD_CONTAINER_HEIGHT = 'auto%';
 const DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM = '24px';
 const DEFAULT_FORM_FIELD_CONTAINER_WIDTH = '100%';
 
@@ -9,13 +10,17 @@ const FORM_FIELD_CONTAINER_NO_MARGIN_BOTTOM = '0px';
 const getFormFieldContainerMarginBottom = (noMargin?: boolean): string =>
   noMargin ? FORM_FIELD_CONTAINER_NO_MARGIN_BOTTOM : DEFAULT_FORM_FIELD_CONTAINER_MARGIN_BOTTOM;
 
+const getFormFieldContainerHeight = (height?: string): string => height ? height : DEFAULT_FORM_FIELD_CONTAINER_HEIGHT;
 const getFormFieldContainerWidth = (width?: string): string => width ? width : DEFAULT_FORM_FIELD_CONTAINER_WIDTH;
 
-export const BodyContainer = Styled(Row)``;
+export const BodyContainer = Styled(Row)`
+  height: 100%;
+`;
 
-export const FormFieldContainer = Styled.div<{ noMargin?: boolean, width?: string }>`
+export const FormFieldContainer = Styled.div<{ height?: string, noMargin?: boolean, width?: string }>`
   position: relative;
 
+  height: ${({ height }): string => getFormFieldContainerHeight(height)};
   margin-bottom: ${({ noMargin }): string => getFormFieldContainerMarginBottom(noMargin)};
   width: ${({ width }): string => getFormFieldContainerWidth(width)};
 `;
