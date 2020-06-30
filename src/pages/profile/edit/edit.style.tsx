f2f6a7582f035b03417b215edfc310fc688edeb5
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-30 11:15:56 -0600

    feat: add profile edit page

---
+++ src/pages/profile/edit/edit.style.tsx
@@ -0,0 +1,9 @@
+import Styled from 'styled-components';
+
+export const EditContainer = Styled.div`
+  height: 100%;
+  padding: 16px;
+  overflow-y: scroll;
+`;
+
+export const BodyContainer = Styled.div``;
