ccddcde03478d558d53a1ac245324b3369e20380
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-30 19:49:20 -0600

    chore: refactor footer

--- src/pages/profile/edit/edit.style.tsx
+++ src/pages/profile/edit/edit.style.tsx
@@ -30,17 +30,3 @@ export const AddButtonContainer = Styled.div`
 export const CurrentContainer = Styled.div`
   margin-top: 48px;
 `;
-
-export const FooterContainer = Styled.div`
-  bottom: 0;
-  display: flex;
-  justify-content: flex-end;
-  padding: 32px;
-  position: fixed;
-  right: 0;
-  width: calc(100% - 32px);
-
-  button:last-child {
-    margin-left: 16px;
-  }
-`;
