0eb73b95db8bd829f9723d2686d3f7034e7d816f
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-30 18:41:04 -0600

    feat: add blog detail skeleton

---
+++ src/pages/blog/detail/detail.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { DetailContainer } from './detail.style';
+
+export const DetailBlogPage: React.FC<IDetailBlog> = () => {
+  return (
+    <DetailContainer></DetailContainer>
+  );
+};
+
+interface IDetailBlog {
+}
