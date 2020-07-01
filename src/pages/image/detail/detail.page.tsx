246257eb5596e60bf82c6565d6460ee127871adb
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-06-30 18:41:55 -0600

    feat: add image detail skeleton

---
+++ src/pages/image/detail/detail.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { DetailContainer } from './detail.style';
+
+export const DetailImagePage: React.FC<IDetailImage> = () => {
+  return (
+    <DetailContainer></DetailContainer>
+  );
+};
+
+interface IDetailImage {
+}
