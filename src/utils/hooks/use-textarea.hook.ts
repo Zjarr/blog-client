3981419af83b7d218bb247b4efd9acf52ccf2027
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-05 15:54:11 -0600

    fix: add optional initial value to textarea hook

--- src/utils/hooks/use-textarea.hook.ts
+++ src/utils/hooks/use-textarea.hook.ts
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export const useTextArea = (inputValue: string): ITextAreaHook => {
-  const [value, setValue] = React.useState<string>(inputValue);
+export const useTextArea = (initialValue?: string): ITextAreaHook => {
+  const [value, setValue] = React.useState<string>(initialValue || '');
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setValue(e.target.value);
