78bb10f6a1c6c2e97d997fc8259c2c2514231c23
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-08 20:32:30 -0600

    feat: add change password modal content

---
+++ src/components/change-password/change-password.style.tsx
@@ -0,0 +1,30 @@
+import Styled from 'styled-components';
+
+import { BORDER_RADIUS_MEDIUM, COLOR_WHITE } from '../../utils/values';
+
+export const ButtonContainer = Styled.div`
+  bottom: 0;
+  display: flex;
+  justify-content: space-between;
+  left: 0;
+  padding: 16px;
+  position: absolute;
+  right: 0;
+`;
+
+export const PasswordContainer = Styled.div`
+  padding: 0 16px;
+`;
+
+export const TitleContainer = Styled.div`
+  padding: 24px 24px 40px;
+`;
+
+export const ChangePasswordContainer = Styled.div`
+  height: 480px;
+  position: relative;
+  width: 480px;
+
+  background-color: ${COLOR_WHITE};
+  border-radius: ${BORDER_RADIUS_MEDIUM};
+`;
