d7e4f83295eed1ab498aaf598a52bd61e52ff933
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-11 21:35:02 -0600

    feat: add skeleton loading box-shadow

--- src/utils/values/box-shadow.value.ts
+++ src/utils/values/box-shadow.value.ts
@@ -1,3 +1,4 @@
-import { COLOR_BLACK_5 } from './colors.value';
+import { COLOR_BLACK_5, COLOR_WHITE_3 } from './colors.value';
 
 export const BOX_SHADOW_OVERLAY = `0 0 8px 0px ${COLOR_BLACK_5}`;
+export const BOX_SHADOW_SKELETON_LOADING = `0 0 8px 8px ${COLOR_WHITE_3}`;
