e8cfc750d498e7497844318784062238e567361e
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-18 18:42:40 -0600

    fix: remove toggle label

--- src/components/toggle/toggle.component.tsx
+++ src/components/toggle/toggle.component.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 
-import { LabelText } from '../text';
-
 import { CheckboxContainer, SliderContainer, ToggleContainer } from './toggle.style';
 
-export const Toggle: React.FC<IToggle> = ({ disabled = false, label, ...rest }) => {
+export const Toggle: React.FC<IToggle> = ({ disabled = false, ...rest }) => {
   return (
-    <>
-      {
-        label && <LabelText>{label}</LabelText>
-      }
-      <ToggleContainer>
-        <CheckboxContainer type={'checkbox'} disabled={disabled} {...rest} />
-        <SliderContainer />
-      </ToggleContainer>
-    </>
+    <ToggleContainer>
+      <CheckboxContainer type={'checkbox'} disabled={disabled} {...rest} />
+      <SliderContainer />
+    </ToggleContainer>
   );
 };
 
 interface IToggle {
   disabled?: boolean;
-  label?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   checked?: boolean;
 }
