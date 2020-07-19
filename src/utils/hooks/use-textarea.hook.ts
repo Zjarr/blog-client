df767c984d29853bbac6e1d4beab1b1b069b4836
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-18 19:40:03 -0600

    fix: add error and value methods

--- src/utils/hooks/use-textarea.hook.ts
+++ src/utils/hooks/use-textarea.hook.ts
@@ -2,20 +2,25 @@ import React from 'react';
 
 export const useTextArea = (initialValue?: string): ITextAreaHook => {
   const [value, setValue] = React.useState<string>(initialValue || '');
+  const [error, setError] = React.useState<string>();
 
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setValue(e.target.value);
   };
 
   return {
+    error,
     onChange,
-    resetValue: (): void => setValue(initialValue || ''),
+    setError,
+    setValue,
     value
   };
 };
 
 export interface ITextAreaHook {
+  error?: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  resetValue: () => void;
+  setError: (error: string) => void;
+  setValue: (value: string) => void;
   value: string;
 }
