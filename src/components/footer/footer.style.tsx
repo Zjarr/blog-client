935b5ad51250a151c011a92937eefe7bcdb83651
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-27 22:06:14 -0600

    feat: update footer responsive design

--- src/components/footer/footer.style.tsx
+++ src/components/footer/footer.style.tsx
@@ -1,14 +1,24 @@
 import Styled from 'styled-components';
 
+import { MEDIA_SM, MEDIA_XL } from '../../utils/values';
+
 export const FooterContainer = Styled.div`
   bottom: 0;
   display: flex;
   justify-content: flex-end;
-  padding: 32px;
+  padding: 16px;
   position: fixed;
   right: 0;
 
   button:not(first-child) {
     margin-left: 16px;
+  }
+
+  ${MEDIA_SM} {
+    padding: 24px;
+  }
+
+  ${MEDIA_XL} {
+    padding: 32px;
   }
 `;
