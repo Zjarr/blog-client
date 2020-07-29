10174bbc38cb1dcf746fe8ffd76a2f8ae0b60fe1
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-28 20:16:23 -0600

    feat: update image update modal responsive design

--- src/components/update-image/update-image.style.tsx
+++ src/components/update-image/update-image.style.tsx
@@ -1,6 +1,15 @@
 import Styled from 'styled-components';
 
-import { BORDER_RADIUS_FULL, BORDER_RADIUS_MEDIUM, COLOR_GRAY_LIGHT, COLOR_PURPLE, COLOR_WHITE } from '../../utils/values';
+import {
+  BORDER_RADIUS_FULL,
+  BORDER_RADIUS_MEDIUM,
+  COLOR_GRAY_LIGHT,
+  COLOR_PURPLE,
+  COLOR_WHITE,
+  MEDIA_MD,
+  TEXT_BIG,
+  TEXT_NORMAL
+} from '../../utils/values';
 
 import { Image } from '../image';
 import { SubtitleText } from '../text';
@@ -23,12 +32,17 @@ export const ButtonContainer = Styled.div`
 
 export const ImageContainer = Styled.div<{ active: boolean }>`
   align-items: center;
+  bottom: 0;
   display: flex;
-  height: 264px;
+  height: 160px;
   justify-content: center;
+  left: 0;
   margin: auto;
+  position: absolute;
+  right: 0;
+  top: 24px;
   transition: 0.25s ease;
-  width: 264px;
+  width: 160px;
 
   border-radius: ${BORDER_RADIUS_FULL};
 
@@ -37,7 +51,12 @@ export const ImageContainer = Styled.div<{ active: boolean }>`
   :hover {
     transition: 0.25s ease;
 
-    border-color: ${COLOR_PURPLE}; 
+    border-color: ${COLOR_PURPLE};
+  }
+
+  ${MEDIA_MD} {
+    height: 264px;
+    width: 264px;
   }
 `;
 
@@ -47,35 +66,66 @@ export const ImageInput = Styled.input`
 
 export const ImageLabel = Styled.label`
   cursor: pointer;
-  height: 240px;
+  height: 136px;
   margin-bottom: 0;
-  width: 240px;
+  width: 136px;
 
   background-color: ${COLOR_GRAY_LIGHT};
   border-radius: ${BORDER_RADIUS_FULL};
+
+  ${MEDIA_MD} {
+    height: 240px;
+    width: 240px;
+  }
 `;
 
 export const ImageResult = Styled(Image)`
-  height: 240px;
+  height: 136px;
   pointer-events: none;
   position: absolute;
-  width: 240px;
+  width: 136px;
+
+  ${MEDIA_MD} {
+    height: 240px;
+    width: 240px;
+  }
 `;
 
 export const ImageText = Styled(SubtitleText)`
   pointer-events: none;
   position: absolute;
+  text-align: center;
+
+  p {
+    font-size: ${TEXT_NORMAL};
+  }
+
+  ${MEDIA_MD} {
+    p {
+      font-size: ${TEXT_BIG};
+    }
+  }
 `;
 
 export const TitleContainer = Styled.div`
-  padding: 24px 24px 40px;
+  padding: 24px 68px 40px 24px;
+
+  ${MEDIA_MD} {
+    padding: 24px 24px 40px;
+  }
 `;
 
 export const UpdateImageContainer = Styled.div`
-  height: 480px;
+  height: calc(100% - 32px);
+  overflow: auto;
   position: relative;
-  width: 480px;
+  width: calc(100% - 32px);
 
   background-color: ${COLOR_WHITE};
   border-radius: ${BORDER_RADIUS_MEDIUM};
+
+  ${MEDIA_MD} {
+    height: 480px;
+    width: 480px;
+  }
 `;
