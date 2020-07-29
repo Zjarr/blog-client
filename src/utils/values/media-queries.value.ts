5ece8257dc351b89859f9735fb2db075b6d3a330
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-07-28 20:33:45 -0600

    feat: update image update modal media queries

--- src/utils/values/media-queries.value.ts
+++ src/utils/values/media-queries.value.ts
@@ -1,3 +1,4 @@
+export const MEDIA_LG = '@media screen and (min-width: 992px)';
 export const MEDIA_MD = '@media screen and (min-width: 768px)';
 export const MEDIA_SM = '@media screen and (min-width: 576px)';
 export const MEDIA_XL = '@media screen and (min-width: 1200px)';
