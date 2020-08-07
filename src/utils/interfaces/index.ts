e4c8e95b44caaf9c81866feb2158368c365da2fb
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-06 22:19:48 -0600

    feat: add error interface

--- src/utils/interfaces/index.ts
+++ src/utils/interfaces/index.ts
@@ -1,5 +1,6 @@
 export * from './category.interface';
 export * from './dropdown-item.interface';
+export * from './error.interface';
 export * from './icon.interface';
 export * from './image-result.interface';
 export * from './social.interface';
