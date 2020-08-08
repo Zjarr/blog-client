3bf1026ec69e27f7c222662fce992421577f72d4
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-07 18:20:01 -0600

    feat: add jwt test for routes

--- src/pages/private/private.page.tsx
+++ src/pages/private/private.page.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useCookies } from 'react-cookie';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
 
+import { jwt } from '../../utils/regexs';
+
 export const PrivatePage: React.FC<RouteProps> = ({ children, ...rest }) => {
+  const [cookies] = useCookies(['authorization']);
+
   return (
     <Route
       render={(): React.ReactNode =>
-        true ? children : <Redirect to={'/admin'} />
+        jwt.test(cookies.authorization) ? children : <Redirect to={'/admin'} />
       }
       {...rest}
     />
--- src/utils/regexs/index.ts
+++ src/utils/regexs/index.ts
@@ -1,1 +1,2 @@
+export * from './jwt.regex';
 export * from './rgba.regex';
