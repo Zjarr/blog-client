9ccf0d291406b9a6ff95aa6e94841f9347358605
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-17 10:43:23 -0600

    feat: add categories list

---
+++ src/pages/category/list/list.graphql.tsx
@@ -0,0 +1,54 @@
+import { DocumentNode, gql } from '@apollo/client/core';
+import { useLazyQuery } from '@apollo/client/react/hooks/useLazyQuery';
+import { QueryTuple } from '@apollo/client/react/types/types';
+
+import { ICategory, IError, IPagination } from '../../../utils/interfaces';
+
+const CATEGORIES_QUERY: DocumentNode = gql`
+  query GetCategories ($categories: GetCategoriesInput!) {
+    categories(categories: $categories) {
+      ... on CategoriesSuccess {
+        categories {
+          _id
+          active
+          description
+          icon
+          name
+        }
+        pagination {
+          limit
+          next
+          page
+          prev
+          total
+        }
+      }
+      ... on Error {
+        error {
+          code
+          message
+          status
+        }
+      }
+    }
+  }
+`;
+
+export interface ICategoriesQueryData {
+  categories: {
+    error?: IError;
+    categories?: ICategory[];
+  }
+}
+
+export interface ICategoriesQueryInput {
+  categories: {
+    active: boolean;
+    name: string;
+    pagination: IPagination;
+  }
+}
+
+export const useCategoriesQuery = (): QueryTuple<ICategoriesQueryData, ICategoriesQueryInput> => {
+  return useLazyQuery<ICategoriesQueryData, ICategoriesQueryInput>(CATEGORIES_QUERY);
+};
--- src/pages/category/list/list.page.tsx
+++ src/pages/category/list/list.page.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import { Banner } from '../../../components/banner';
 import { SimpleButton } from '../../../components/button';
+import { IImageCard } from '../../../components/card';
 import { Footer } from '../../../components/footer';
 import { FormField } from '../../../components/form-field';
 import { Header } from '../../../components/header';
@@ -8,13 +10,94 @@ import { Input } from '../../../components/input';
 import { List } from '../../../components/list';
 import { SubtitleText } from '../../../components/text';
 import { Toggle } from '../../../components/toggle';
-import { useNavigateTo } from '../../../utils/hooks';
-import { COLOR_PURPLE } from '../../../utils/values';
+import { useInput, useNavigateTo } from '../../../utils/hooks';
+import { ICategory } from '../../../utils/interfaces';
+import { COLOR_PURPLE, COLOR_RED, PAGINATION_DEFAULT, STRING_SERVER_ERROR } from '../../../utils/values';
 
+import { ICategoriesQueryData, useCategoriesQuery } from './list.graphql';
 import { BodyContainer, CategoryListContainer, FilterContainer, ListContainer } from './list.style';
 
 export const ListCategoryPage: React.FC<IListCategoryPage> = () => {
   const navigateTo = useNavigateTo();
+
+  const [categoriesQuery, {
+    error: categoriesQueryError,
+    data: categoriesQueryData,
+    loading: categoriesQueryLoading
+  }] = useCategoriesQuery();
+
+  const [bannerVisible, setBannerVisible] = React.useState<boolean>(false);
+  const [bannerMessage, setBannerMessage] = React.useState<string>('');
+  const [categories, setCategories] = React.useState<IImageCard[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  const filterActive = useInput('', true);
+  const filterSearch = useInput('');
+
+  const handleBannerMessageHide = (): void => {
+    return setBannerVisible(false);
+  };
+
+  const showBannerMessage = (message: string): void => {
+    setBannerMessage(message);
+
+    return setBannerVisible(true);
+  };
+
+  const buildCategoriesObject = React.useCallback((categories: ICategory[]) => {
+    const categoryCards: IImageCard[] = [];
+
+    categories.forEach(category => {
+      const newCategoryCard: IImageCard = {
+        active: category.active,
+        icon: category.icon,
+        link: `/admin/categories/view/${category._id}`,
+        primaryText: category.description,
+        primaryTextIcon: 'description',
+        secondaryText: category.icon,
+        secondaryTextIcon: 'category',
+        title: category.name
+      };
+
+      categoryCards.push(newCategoryCard);
+    });
+
+    setLoading(false);
+
+    return setCategories(categoryCards);
+  }, []);
+
+  const getCategories = React.useCallback((active: boolean, name: string) => {
+    categoriesQuery({
+      variables: {
+        categories: {
+          active,
+          name,
+          pagination: PAGINATION_DEFAULT
+        }
+      }
+    });
+  }, [categoriesQuery]);
+
+  const handleCategoriesQueryResponse = React.useCallback((data: ICategoriesQueryData): void => {
+    const { error, categories } = data.categories;
+
+    if (error) return showBannerMessage(error.message);
+    if (!categories) return;
+
+    return buildCategoriesObject(categories);
+  }, [buildCategoriesObject]);
+
+  React.useEffect(() => {
+    if (categoriesQueryLoading || categoriesQueryError) return;
+
+    return getCategories(filterActive.checked, filterSearch.value);
+  }, [categoriesQueryError, categoriesQueryLoading, filterActive.checked, filterSearch.value, getCategories]);
+
+  React.useEffect(() => {
+    if (categoriesQueryError) return showBannerMessage(STRING_SERVER_ERROR);
+    if (categoriesQueryData) return handleCategoriesQueryResponse(categoriesQueryData);
+  }, [categoriesQueryData, categoriesQueryError, handleCategoriesQueryResponse]);
 
   return (
     <CategoryListContainer>
@@ -22,18 +105,18 @@ export const ListCategoryPage: React.FC<IListCategoryPage> = () => {
 
       <BodyContainer>
         <ListContainer xl={9} position={'left'}>
-          <List loading={false} cards={[]} />
+          <List loading={categoriesQueryLoading || loading} cards={categories} error={!!categoriesQueryError} />
         </ListContainer>
 
         <FilterContainer xl={3} position={'right'}>
           <SubtitleText icon={'filter_list'}>Filter</SubtitleText>
 
           <FormField label={'Search:'}>
-            <Input icon={'search'} placeholder={'Category name'} />
+            <Input icon={'search'} placeholder={'Category name'} {...filterSearch} />
           </FormField>
 
           <FormField label={'Active:'}>
-            <Toggle />
+            <Toggle {...filterActive} />
           </FormField>
         </FilterContainer>
       </BodyContainer>
@@ -41,6 +124,13 @@ export const ListCategoryPage: React.FC<IListCategoryPage> = () => {
       <Footer>
         <SimpleButton color={COLOR_PURPLE} icon={'add'} onClick={(): void => navigateTo('/admin/categories/add')} />
       </Footer>
+
+      <Banner
+        color={COLOR_RED}
+        icon={'clear'}
+        onHide={handleBannerMessageHide}
+        text={bannerMessage}
+        visible={bannerVisible} />
     </CategoryListContainer>
   );
 };
