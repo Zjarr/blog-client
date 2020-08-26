61309cb1b53165c568451eec43f44642a9e24eae
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-26 10:25:58 -0600

    feat: add empty container

---
+++ src/components/empty/empty.component.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import Broken from '../../assets/images/broken.png';
+import Rocket from '../../assets/images/rocket.png';
+
+import { COLOR_GRAY_MEDIUM } from '../../utils/values';
+
+import { ParagraphText } from '../text';
+
+import {
+  EmptyContainer,
+  ImageContainer,
+  RocketImage,
+  TextContainer
+} from './empty.style';
+
+export const Empty: React.FC<IEmpty> = ({ error }) => {
+  return (
+    <EmptyContainer>
+      <ImageContainer>
+        <RocketImage src={error ? Broken : Rocket} />
+      </ImageContainer>
+
+      <TextContainer>
+        <ParagraphText color={COLOR_GRAY_MEDIUM}>Oops!</ParagraphText>
+        <ParagraphText color={COLOR_GRAY_MEDIUM}>
+          {
+            error ? 'We ran into an error. Please try again.' : 'This looks empty. Did you add some fuel?'
+          }
+        </ParagraphText>
+      </TextContainer>
+    </EmptyContainer>
+  );
+};
+
+interface IEmpty {
+  error?: boolean;
+}
--- src/components/list/list.style.tsx
+++ src/components/list/list.style.tsx
@@ -13,31 +13,3 @@ export const ListContainer = Styled(Row)`
 export const CardContainer = Styled(Column)`
   margin-bottom: 32px;
 `;
-
-export const EmptyListContainer = Styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  justify-content: center;
-  width: 100%;
-`;
-
-export const ImageContainer = Styled.div`
-  height: 180px;
-  margin: 0px 0px 16px;
-  width: 180px;
-`;
-
-export const RocketImage = Styled.img`
-  height: 100%;
-  object-fit: contain;
-  width: 100%;
-`;
-
-export const TextContainer = Styled.div`
-  * {
-    text-align: center;
-    width: 100%;
-  }
-`;
