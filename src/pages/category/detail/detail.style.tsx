b6f09730c48875e9f64a7c4dbed15fcc236d65f4
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-26 11:09:49 -0600

    feat: add not found state

--- src/pages/category/detail/detail.style.tsx
+++ src/pages/category/detail/detail.style.tsx
@@ -3,6 +3,12 @@ import Styled from 'styled-components';
 
 import { MEDIA_SM, MEDIA_XL } from '../../../utils/values';
 
+const DEFAULT_BODY_HEIGHT = 'auto';
+
+const EMPTY_BODY_HEIGHT = 'calc(100% - 112px)';
+
+const getBodyheight = (empty?: boolean): string => empty ? EMPTY_BODY_HEIGHT : DEFAULT_BODY_HEIGHT;
+
 export const DetailContainer = Styled.div`
   height: 100%;
   padding: 16px 16px 80px;
@@ -17,4 +23,8 @@ export const DetailContainer = Styled.div`
   }
 `;
 
-export const BodyContainer = Styled(Row)``;
+export const BodyContainer = Styled(Row) <{ empty: boolean }>`
+  align-items: center;
+
+  min-height: ${({ empty }): string => getBodyheight(empty)};
+`;
