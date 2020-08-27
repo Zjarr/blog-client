4e0d53c4e4b6a7ed09fa8976f997345f4eb531b0
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-27 12:11:42 -0600

    feat: add image detail be

--- src/pages/image/detail/detail.style.tsx
+++ src/pages/image/detail/detail.style.tsx
@@ -4,6 +4,12 @@ import Styled from 'styled-components';
 import { Column } from '../../../components/column';
 import { MEDIA_SM, MEDIA_XL } from '../../../utils/values';
 
+const DEFAULT_BODY_HEIGHT = 'auto';
+
+const EMPTY_BODY_HEIGHT = 'calc(100% - 112px)';
+
+const getBodyheight = (empty?: boolean): string => empty ? EMPTY_BODY_HEIGHT : DEFAULT_BODY_HEIGHT;
+
 export const DetailContainer = Styled.div`
   height: 100%;
   padding: 16px 16px 80px;
@@ -18,7 +24,11 @@ export const DetailContainer = Styled.div`
   }
 `;
 
-export const BodyContainer = Styled(Row)``;
+export const BodyContainer = Styled(Row) <{ empty: boolean }>`
+  align-items: center;
+
+  min-height: ${({ empty }): string => getBodyheight(empty)};
+`;
 
 export const ImageColumn = Styled(Column)`
   align-items: center;
