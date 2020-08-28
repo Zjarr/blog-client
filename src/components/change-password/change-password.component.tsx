367a1b9ba42492c08769e31d70147e13ca9bc5dc
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-27 23:34:25 -0600

    feat: update password modal be

--- src/components/change-password/change-password.component.tsx
+++ src/components/change-password/change-password.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { COLOR_PURPLE } from '../../utils/values';
+import { useInput } from '../../utils/hooks';
+import { COLOR_PURPLE, STRING_FIELD_REQUIRED } from '../../utils/values';
 
 import { SimpleButton } from '../button';
 import { FormField } from '../form-field';
@@ -16,8 +17,48 @@ import {
 } from './change-password.style';
 
 export const ChangePassword: React.FC<IChangePassword> = ({ onClose, visible }) => {
+  const currentPassword = useInput('');
+  const repeatPassword = useInput('');
+  const updatedPassword = useInput('');
+
+  const clearInputs = (): void => {
+    currentPassword.setValue('');
+    repeatPassword.setValue('');
+    updatedPassword.setValue('');
+
+    currentPassword.setError('');
+    repeatPassword.setError('');
+    updatedPassword.setError('');
+  };
+
+  const isValidForm = (): boolean => {
+    if (!currentPassword.value) currentPassword.setError(STRING_FIELD_REQUIRED);
+    if (!repeatPassword.value) repeatPassword.setError(STRING_FIELD_REQUIRED);
+    if (!updatedPassword.value) updatedPassword.setError(STRING_FIELD_REQUIRED);
+
+    if (!currentPassword.value || !repeatPassword.value || !updatedPassword.value) return false;
+
+    if (updatedPassword.value !== repeatPassword.value) {
+      repeatPassword.setError('Password does not match');
+
+      return false;
+    }
+
+    return true;
+  };
+
   const handleCloseRequest = (): void => {
-    return onClose && onClose();
+    if (onClose) onClose(null);
+
+    return clearInputs();
+  };
+
+  const handlePasswordUpdateClick = (): void => {
+    if (!isValidForm()) return;
+
+    if (onClose) onClose({ current: currentPassword.value, updated: updatedPassword.value });
+
+    return clearInputs();
   };
 
   return (
@@ -29,20 +70,35 @@ export const ChangePassword: React.FC<IChangePassword> = ({ onClose, visible }) 
 
         <PasswordContainer>
           <FormField label={'Current password:'}>
-            <Input icon={'vpn_key'} placeholder={'Your current password'} />
+            <Input
+              icon={'vpn_key'}
+              placeholder={'Your current password'}
+              type={'password'}
+              {...currentPassword} />
           </FormField>
 
           <FormField label={'New password:'}>
-            <Input icon={'vpn_key'} placeholder={'Your new password'} />
+            <Input
+              icon={'vpn_key'}
+              placeholder={'Your new password'}
+              type={'password'}
+              {...updatedPassword} />
           </FormField>
 
           <FormField label={'Repeat new password:'}>
-            <Input icon={'vpn_key'} placeholder={'Repeat your new password'} />
+            <Input
+              icon={'vpn_key'}
+              placeholder={'Repeat your new password'}
+              type={'password'}
+              {...repeatPassword} />
           </FormField>
         </PasswordContainer>
 
         <ButtonContainer>
-          <SimpleButton icon={'done'} color={COLOR_PURPLE} />
+          <SimpleButton
+            color={COLOR_PURPLE}
+            icon={'done'}
+            onClick={handlePasswordUpdateClick} />
         </ButtonContainer>
       </ChangePasswordContainer>
     </Modal>
@@ -51,5 +107,5 @@ export const ChangePassword: React.FC<IChangePassword> = ({ onClose, visible }) 
 
 interface IChangePassword {
   visible: boolean;
-  onClose: () => void;
+  onClose: (password: { current: string; updated: string } | null) => void;
 }
