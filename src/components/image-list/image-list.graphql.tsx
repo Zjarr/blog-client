ea55c50313688b51559a4bb0d7a880985ea56f26
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-08-28 13:04:45 -0600

    feat: add images modal be

---
+++ src/components/image-list/image-list.graphql.tsx
@@ -0,0 +1,55 @@
+import { DocumentNode, gql } from '@apollo/client/core';
+import { useLazyQuery } from '@apollo/client/react/hooks/useLazyQuery';
+import { QueryTuple } from '@apollo/client/react/types/types';
+
+import { IError, IImage, IPagination } from '../../utils/interfaces';
+
+const IMAGES_QUERY: DocumentNode = gql`
+  query Images($images: GetImagesInput!) {
+    images(images: $images) {
+      ... on ImagesSuccess {
+        images {
+          _id
+          active
+          alt
+          description
+          name
+          url
+        }
+        pagination {
+          limit
+          next
+          page
+          prev
+          total
+        }
+      }
+      ... on Error {
+        error {
+          code
+          message
+          status
+        }
+      }
+    }
+  }
+`;
+
+export interface IImagesQueryData {
+  images: {
+    error?: IError;
+    images?: IImage[];
+  }
+}
+
+export interface IImagesQueryInput {
+  images: {
+    active: boolean;
+    name: string;
+    pagination: IPagination;
+  }
+}
+
+export const useImagesQuery = (): QueryTuple<IImagesQueryData, IImagesQueryInput> => {
+  return useLazyQuery<IImagesQueryData, IImagesQueryInput>(IMAGES_QUERY, { fetchPolicy: 'cache-and-network' });
+};
