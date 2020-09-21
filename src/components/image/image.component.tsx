85387b355d3a0144c75ee07d5215196feb5c0cc6
Pablo Navarro <j.pablonavarro95@gmail.com>
2020-09-20 19:59:14 -0600

    feat: add image error border

--- src/components/image/image.component.tsx
+++ src/components/image/image.component.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
 import ImagePlaceholder from '../../assets/images/img-placeholder.png';
-
 import { Icon } from '../icon';
 
-import { ImageContainer, ImageUpdateButton, Img } from './image.style';
+import { ImageContainer, ImageErrorBorder, ImageUpdateButton, Img } from './image.style';
 
-export const Image: React.FC<IImage> = ({ alt, onUpdateClick, shape, src, updatable = false, ...rest }) => {
+export const Image: React.FC<IImage> = ({ alt, error, onUpdateClick, shape, src, updatable = false, ...rest }) => {
   const handleUpdateImageClick = (): void => {
     return onUpdateClick && onUpdateClick();
   };
@@ -14,6 +13,10 @@ export const Image: React.FC<IImage> = ({ alt, onUpdateClick, shape, src, updata
   return (
     <ImageContainer updatable={updatable} shape={shape} {...rest}>
       <Img alt={alt} noImg={!src} src={src || ImagePlaceholder} {...rest} />
+
+      {
+        error && <ImageErrorBorder />
+      }
 
       <ImageUpdateButton disabled={!updatable} onClick={handleUpdateImageClick} shape={shape}>
         <Icon name={'edit'} size={'32px'} />
@@ -24,6 +27,7 @@ export const Image: React.FC<IImage> = ({ alt, onUpdateClick, shape, src, updata
 
 interface IImage {
   alt?: string;
+  error?: boolean;
   height?: string;
   onUpdateClick?: () => void,
   shape: 'circle' | 'square';
--- src/components/image/image.style.tsx
+++ src/components/image/image.style.tsx
@@ -1,17 +1,19 @@
 import Styled from 'styled-components';
 
-import { BORDER_RADIUS_FULL, BORDER_RADIUS_SMALL, COLOR_BLACK, COLOR_GRAY_LIGHT, COLOR_WHITE } from '../../utils/values';
+import { BORDER_RADIUS_FULL, BORDER_RADIUS_SMALL, COLOR_BLACK, COLOR_GRAY_LIGHT, COLOR_RED, COLOR_WHITE } from '../../utils/values';
 
 const CIRCLE_IMAGE_CONTAINER_BORDER_RADIUS = BORDER_RADIUS_FULL;
 
 const DEFAULT_IMAGE_CONTAINER_BORDER_RADIUS = BORDER_RADIUS_SMALL;
 const DEFAULT_IMAGE_CONTAINER_SIZE = '96px';
 
+const DEFAULT_IMAGE_BORDER_RADIUS = BORDER_RADIUS_FULL;
 const DEFAULT_IMAGE_HEIGHT = '100%';
 const DEFAULT_IMAGE_OBJECT_FIT = 'cover';
 const DEFAULT_IMAGE_WIDTH = '100%';
 const DEFAULT_UPDATE_BUTTON_OPACITY = '1';
 
+const NO_IMAGE_BORDER_RADIUS = BORDER_RADIUS_SMALL;
 const NO_IMAGE_HEIGHT = '65%';
 const NO_IMAGE_OBJECT_FIT = 'container';
 const NO_IMAGE_WIDTH = 'auto';
@@ -23,6 +25,7 @@ const getContainerBorderRadius = (shape?: string): string => shape === 'circle' 
 
 const getContainerSize = (size?: string): string => size ? size : DEFAULT_IMAGE_CONTAINER_SIZE;
 
+const getImageBorderRadius = (noImg?: boolean): string => noImg ? NO_IMAGE_BORDER_RADIUS : DEFAULT_IMAGE_BORDER_RADIUS;
 const getImageHeight = (noImg?: boolean): string => noImg ? NO_IMAGE_HEIGHT : DEFAULT_IMAGE_HEIGHT;
 const getImageObjectFit = (noImg?: boolean): string => noImg ? NO_IMAGE_OBJECT_FIT : DEFAULT_IMAGE_OBJECT_FIT;
 const getImageWidth = (noImg?: boolean): string => noImg ? NO_IMAGE_WIDTH : DEFAULT_IMAGE_WIDTH;
@@ -59,15 +62,16 @@ export const ImageContainer = Styled.div<{
 }>`
   align-items: center;
   display: flex;
+  height: var(--image-container-height);
   justify-content: center;
-  overflow: hidden;
   position: relative;
+  width: var(--image-container-width);
 
   background-color: ${COLOR_GRAY_LIGHT};
 
   border-radius: ${({ shape }): string => getContainerBorderRadius(shape)};
-  height: ${({ height }): string => getContainerSize(height)};
-  width: ${({ width }): string => getContainerSize(width)};
+  --image-container-height: ${({ height }): string => getContainerSize(height)};
+  --image-container-width: ${({ width }): string => getContainerSize(width)};
 
   ${ImageUpdateButton} {
     opacity: 0;
@@ -81,7 +85,17 @@ export const ImageContainer = Styled.div<{
   }
 `;
 
+export const ImageErrorBorder = Styled.div`
+  height: calc(var(--image-container-height) + 20px);
+  position: absolute;
+  width: calc(var(--image-container-width) + 20px);
+
+  border: 4px solid ${COLOR_RED};
+  border-radius: ${BORDER_RADIUS_FULL};
+`;
+
 export const Img = Styled.img<{ noImg?: boolean }>`
+  border-radius: ${({ noImg }): string => getImageBorderRadius(noImg)};
   height: ${({ noImg }): string => getImageHeight(noImg)};
   object-fit: ${({ noImg }): string => getImageObjectFit(noImg)};
   width: ${({ noImg }): string => getImageWidth(noImg)};
